301ebc07efbbb921eb78cdf09344cf74bcd18301
Ildar Sadykov <ismarxovich@gmail.com>
2021-11-18 12:45:06 +0200

    install Sage and Builded Tailwind CSS

---
+++ resources/assets/styles/tailwind.config.js
@@ -0,0 +1,29 @@
+/*
+|-------------------------------------------------------------------------------
+| Tailwind – The Utility-First CSS Framework
+|-------------------------------------------------------------------------------
+|
+| Documentation at https://tailwindcss.com
+|
+*/
+
+/**
+ * Global Styles Plugin
+ *
+ * This plugin modifies Tailwind’s base styles using values from the theme.
+ * https://tailwindcss.com/docs/adding-base-styles#using-a-plugin
+ */
+
+/**
+ * Configuration
+ */
+module.exports = {
+  purge: ['resources/views/*.blade.php'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
