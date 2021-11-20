7bb2cc8f6968cd4b521b1432ac910f156c2ffa74
Ildar Sadykov <ismarxovich@gmail.com>
2021-11-20 20:53:56 +0200

    add styles for wysiwyg editor styles

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,2 +1,6 @@
-@php the_content() @endphp
+<div class="container mx-auto">
+    <div class="wysiwyg">
+        @php the_content() @endphp
+    </div>
+</div>
 {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
