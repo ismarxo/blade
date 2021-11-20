ab27e47a611a38e5e600c62f93f3ecd12d0aa6e5
Ildar Sadykov <ismarxovich@gmail.com>
2021-11-20 20:54:28 +0200

    add test Tailwind styles templates

---
+++ resources/views/sections/hero/right-image.blade.php
@@ -0,0 +1,54 @@
+<div class="container mx-auto">
+  <!-- hero start -->
+  <div id="hero" class="relative z-0 py-14 md:py-16 bg-white dark:bg-gray-800 overflow-hidden">
+    <div class="container xl:max-w-7xl mx-auto px-4">
+      <!-- row -->
+      <div class="flex flex-wrap flex-row -mx-4 justify-center">
+        <!-- hero content -->
+        <div class="flex-shrink max-w-full px-4 w-full md:w-9/12 lg:w-1/2 self-center lg:pr-12">
+          <div class="text-center lg:text-left mt-6 lg:mt-0 wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">
+            <div class="mb-4">
+              <span class="text-sm py-1 px-2 bg-blue-700 text-gray-100 rounded">100%</span>
+              <span class="ml-1">Satisfaction Guarantee</span>
+            </div>
+            <div class="mb-12">
+              <h1 class="text-4xl leading-normal mb-2 font-bold text-gray-800 dark:text-gray-100">A <span class="text-blue-700">Digital Agency</span> that will help your business.
+              </h1>
+              <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">We help brands grow their online presence, dominate their niche, and make more sales.</p>
+            </div>
+            <a class="py-3 px-5 inline-block text-center rounded-md leading-normal text-gray-100 bg-blue-700 border border-blue-700 hover:text-white hover:bg-blue-800 hover:ring-0 hover:border-blue-800 focus:bg-blue-800 focus:border-blue-800 focus:outline-none focus:ring-0 mr-4" href="#about-us">
+              <svg xmlns="http://www.w3.org/2000/svg" class="inline-block mr-1" width="1.5rem" height="1.5rem" fill="currentColor" viewBox="0 0 512 512"><polyline points="336 176 225.2 304 176 255.8" style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><path d="M463.1,112.37C373.68,96.33,336.71,84.45,256,48,175.29,84.45,138.32,96.33,48.9,112.37,32.7,369.13,240.58,457.79,256,464,271.42,457.79,479.3,369.13,463.1,112.37Z" style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>
+              About Us
+            </a>
+            <a class="py-3 px-5 inline-block text-center rounded-md leading-normal text-gray-100 bg-gray-900 border border-gray-900 hover:text-white hover:bg-black hover:ring-0 hover:border-black focus:bg-black focus:border-black focus:outline-none focus:ring-0" href="#service">
+              <svg xmlns="http://www.w3.org/2000/svg" class="inline-block mr-1" width="1.5rem" height="1.5rem" fill="currentColor" viewBox="0 0 512 512"><rect x="64" y="176" width="384" height="256" rx="28.87" ry="28.87" style="fill:none;stroke:currentColor;stroke-linejoin:round;stroke-width:32px"/><line x1="144" y1="80" x2="368" y2="80" style="stroke:currentColor;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px"/><line x1="112" y1="128" x2="400" y2="128" style="stroke:currentColor;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px"/></svg>
+               Our Service
+            </a>
+          </div>
+        </div>
+  
+        <!-- hero image -->
+        <div class="flex-shrink max-w-full px-4 w-full md:w-9/12 lg:w-1/2 self-center">
+          <div class="px-12 md:ml-16 md:pr-0 md:mt-0 wow fadeInRight" data-wow-duration="1s" data-wow-delay=".1s">
+            <figure class="relative">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+                <!-- Path back image-->
+                <path class="text-gray-100" fill="currentColor" d="M51.2,-53.5C67.6,-47.4,82.7,-32.3,85.6,-15.2C88.6,2,79.2,21.2,67.7,37.2C56.2,53.3,42.5,66.2,26.7,70.8C11,75.4,-6.9,71.6,-19.6,63C-32.4,54.4,-40,41,-48.6,27.4C-57.2,13.9,-66.7,0.2,-68.1,-15.5C-69.5,-31.3,-62.8,-49.1,-50.1,-55.9C-37.5,-62.7,-18.7,-58.4,-0.6,-57.6C17.5,-56.9,34.9,-59.7,51.2,-53.5Z" transform="translate(100 100)" />
+                <!-- Clip path image -->
+                <defs>
+                  <clipPath id="svg-mask1">
+                    <path fill="currentColor" d="M39.2,-57.5C49.8,-46.3,56.6,-33.5,62.7,-19.3C68.7,-5.2,73.9,10.3,71.6,25C69.2,39.8,59.3,53.9,46.1,58C32.9,62.2,16.5,56.3,1.2,54.7C-14.2,53.1,-28.3,55.8,-42.1,51.9C-56,48,-69.5,37.5,-68.6,25.7C-67.7,14,-52.3,1,-45.9,-13.6C-39.4,-28.2,-41.7,-44.5,-35.6,-57.1C-29.4,-69.7,-14.7,-78.7,-0.2,-78.4C14.3,-78.1,28.6,-68.6,39.2,-57.5Z" transform="translate(100 100)" />
+                  </clipPath>
+                </defs>
+                <!-- Source image -->
+                <image transform="matrix(1 0 0 1 0 0)" clip-path="url(#svg-mask1)" xlink:href="src/img-min/human/hero-agency.jpg" width="235" height="200"></image>
+              </svg>
+            </figure>
+          </div>
+        </div>
+      </div><!-- end row -->
+    </div>
+  </div>
+  <!-- end hero -->
+</div>
+  
