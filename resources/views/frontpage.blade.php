d56772f4ce0e479f118ca282994a4eb67fe4e746
Ildar Sadykov <ismarxovich@gmail.com>
2021-11-20 20:54:48 +0200

    update templates

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,11 +1,16 @@
 {{--
-  Template Name: Custom Template
+  Template Name: FrontPage Template
 --}}
 
 @extends('layouts.app')
 
 @section('content')
+  @include('sections.hero.right-image')
+  @include('test.font-weight')
+  @include('test.font-size')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-page')
   @endwhile
 @endsection
+
+
