99214f8a94f572aefaa8510143bd5fddbbf9bf70
Ildar Sadykov <ismarxovich@gmail.com>
2021-11-21 12:59:36 +0200

    update base layout structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,27 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
-  @include('partials.head')
-  <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>        
+@include('partials.head')
+
+<body @php body_class() @endphp>
+  <div id="body-wrapper">
+    <div id="header-wrapper">
+      @php do_action('get_header') @endphp
+      @include('partials.header')
+    </div>
+    <div id="main-wrapper">
+      <aside id="sidebar">
+
+      </aside>
+      <div id="content-wrapper" role="document">
+        @yield('content')
       </div>
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
-  </body>
+    <div id="footer-wrapper">
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')      
+    </div>   
+  </div>
+  @php wp_footer() @endphp
+</body>
+
 </html>
