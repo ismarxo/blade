32586e5acb3643eb62de25cfaefb287fc0579159
Ildar Sadykov <ismarxovich@gmail.com>
2021-12-07 13:17:41 +0200

    update SectionHero's model, controller, view

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,8 +1,24 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    // JavaScript to be fired on all pages  
+    
+    /**
+     * Sections scripts 
+     *
+     */
+
+    // 1. hero sections
+
+    // 1.1. heroSimple
+
+    $('#hero-slider').slick({
+      infinite: true,
+      arrows: false,
+      dots: false,
+    });   
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+    
   },
 };
---
+++ resources/assets/scripts/routes/sectionHero.js
@@ -0,0 +1,10 @@
+export default {
+    init() {
+      // JavaScript to be fired on the home page
+      $('#hero-slider').slick();
+    },
+    finalize() {
+      // JavaScript to be fired on the home page, after the init JS
+    },
+  };
+  
